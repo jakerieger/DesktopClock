a3176350f155bb486d97ed3d20c881081f00fe89
Jake Rieger <contact.jakerieger@gmail.com>
2024-08-01 09:05:58 -0400

    update app icon

--- DesktopClock/DesktopClock.cpp
+++ DesktopClock/DesktopClock.cpp
@@ -281,7 +281,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance) {
     wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     wcex.lpszMenuName  = nullptr;
     wcex.lpszClassName = szWindowClass;
-    wcex.hIconSm       = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.hIconSm       = wcex.hIcon;
 
     return RegisterClassExW(&wcex);
 }
